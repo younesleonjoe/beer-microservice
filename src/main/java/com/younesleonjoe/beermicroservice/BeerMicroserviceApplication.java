e64d8c2caba3f4b61e0a9d4915731da625fc77b9
younesleonjoe <leonjoeyounes21@gmail.com>
2024-03-25 20:49:08 +0200

    Initial commit

---
+++ src/main/java/com/younesleonjoe/beermicroservice/BeerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.younesleonjoe.beermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younesleonjoe/beermicroservice/BeerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younesleonjoe.beermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
