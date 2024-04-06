f13edbca1c2bde18fccf31ff2ee4a380518aaeee
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-06 22:42:17 +0300

    Create Beer entity v1

---
+++ src/main/java/com/younesleonjoe/beermicroservice/beer/v1/Beer.java
@@ -0,0 +1,43 @@
+package com.younesleonjoe.beermicroservice.beer.v1;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(columnDefinition = "varchar(36)", updatable = false, nullable = false)
+  @JdbcTypeCode(SqlTypes.VARCHAR)
+  private UUID id;
+
+  @Version private Integer version;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Timestamp createdAt;
+
+  @UpdateTimestamp private Timestamp updatedAt;
+
+  private String beerName;
+
+  private String beerStyle;
+
+  @Column(unique = true)
+  private Long upc;
+
+  private BigDecimal price;
+  private Integer quantityOnHand;
+}
