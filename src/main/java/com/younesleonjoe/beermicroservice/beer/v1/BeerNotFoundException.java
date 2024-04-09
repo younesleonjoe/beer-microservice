7aeda74258d53fd1ba58155d663dc6b9675b6347
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-10 02:15:00 +0300

    Create BeerNotFoundException

---
+++ src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.younesleonjoe.beermicroservice.beer.v1;
+
+import java.util.UUID;
+
+public class BeerNotFoundException extends RuntimeException {
+  public BeerNotFoundException(UUID id) {
+    super(String.format("Beer with id %s not found", id));
+  }
+}
