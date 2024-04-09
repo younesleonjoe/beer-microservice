a3886b48354d8780004fb2d82f1f7cd22bd4e70c
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-10 02:16:16 +0300

    Edit BeerServiceImpl
    Added BeerRepository and BeerMapper and used them in each implementation

--- src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerServiceImpl.java
+++ src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerServiceImpl.java
@@ -3,10 +3,16 @@ package com.younesleonjoe.beermicroservice.beer.v1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BeerServiceImpl implements BeerService {
+
+  private final BeerRepository beerRepository;
+  private final BeerMapper beerMapper;
+
   private List<BeerDTO> generateListOfEmptyBeers() {
     List<BeerDTO> beerDTOList = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
@@ -26,16 +32,22 @@ public class BeerServiceImpl implements BeerService {
 
   @Override
   public BeerDTO findById(UUID id) {
-    return generateEmptyBeer(id);
+    // return generateEmptyBeer(id);
+    return beerMapper.beerToBeerDTO(
+        beerRepository.findById(id).orElseThrow(() -> new BeerNotFoundException(id)));
   }
 
   @Override
   public BeerDTO create(BeerDTO beerDTO) {
-    return beerDTO;
+    // return beerDTO;
+    return beerMapper.beerToBeerDTO(beerRepository.save(beerMapper.beerDTOToBeer(beerDTO)));
   }
 
   @Override
-  public void update(UUID id, BeerDTO beerDTO) {}
+  public void update(UUID id, BeerDTO beerDTO) {
+    beerRepository.findById(id).orElseThrow(() -> new BeerNotFoundException(id));
+    beerRepository.save(beerMapper.beerDTOToBeer(beerDTO));
+  }
 
   @Override
   public void delete(UUID id) {}
