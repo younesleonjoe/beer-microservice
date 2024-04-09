a589abd0ad2cfb56ea055dfbf3125205c5a48a39
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-10 01:14:29 +0300

    Edit BeerDTO
    Added Jackson Config @JsonFormat for createdAt, updatedAt, and price

--- src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerDTO.java
+++ src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerDTO.java
@@ -1,13 +1,13 @@
 package com.younesleonjoe.beermicroservice.beer.v1;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
-
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,11 +20,23 @@ import lombok.NoArgsConstructor;
 public class BeerDTO {
   @Null private UUID id;
   @Null private Integer version;
-  @Null private OffsetDateTime createdAt;
-  @Null private OffsetDateTime updatedAt;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ'", shape = JsonFormat.Shape.STRING)
+  @Null
+  private OffsetDateTime createdAt;
+
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ'", shape = JsonFormat.Shape.STRING)
+  @Null
+  private OffsetDateTime updatedAt;
+
   @NotBlank private String beerName;
   @NotNull private BeerStyleEnum beerStyle;
   @Positive @NotNull private Long upc;
-  @Positive @NotNull private BigDecimal price;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  @Positive
+  @NotNull
+  private BigDecimal price;
+
   private Integer quantityOnHand;
 }
