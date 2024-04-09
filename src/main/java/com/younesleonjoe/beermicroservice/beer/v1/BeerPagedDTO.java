7df1f9db99ce425a2dad9f0881a11275a69e3236
younesleonjoe <leonjoeyounes21@gmail.com>
2024-04-10 01:30:33 +0300

    Edit BeerPagedDTO
    Added Jackson JsonCreator

--- src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerPagedDTO.java
+++ src/main/java/com/younesleonjoe/beermicroservice/beer/v1/BeerPagedDTO.java
@@ -1,16 +1,36 @@
 package com.younesleonjoe.beermicroservice.beer.v1;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public class BeerPagedDTO extends PageImpl<BeerDTO> {
-	public BeerPagedDTO(List<BeerDTO> content, Pageable pageable, long total) {
-		super(content, pageable, total);
-	}
 
-	public BeerPagedDTO(List<BeerDTO> content) {
-		super(content);
-	}
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+  public BeerPagedDTO(
+      @JsonProperty("content") List<BeerDTO> content,
+      @JsonProperty("number") int number,
+      @JsonProperty("size") int size,
+      @JsonProperty("totalElements") long totalElements,
+      @JsonProperty("pageable") JsonNode pageable,
+      @JsonProperty("last") boolean last,
+      @JsonProperty("totalPages") long totalPages,
+      @JsonProperty("sort") JsonNode sort,
+      @JsonProperty("first") boolean first,
+      @JsonProperty("numberOfElements") int numberOfElements) {
+
+    super(content, PageRequest.of(number, size), totalElements);
+  }
+
+  public BeerPagedDTO(List<BeerDTO> content, Pageable pageable, long total) {
+    super(content, pageable, total);
+  }
+
+  public BeerPagedDTO(List<BeerDTO> content) {
+    super(content);
+  }
 }
